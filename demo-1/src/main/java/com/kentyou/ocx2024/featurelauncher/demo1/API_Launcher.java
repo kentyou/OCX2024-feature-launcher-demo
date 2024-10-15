7a18567e1ca56d165b78337525c5ae885ccd500d
Tim Ward <timothyjward@apache.org>
2024-10-15 16:38:58 +0100

    Initial demo code

---
+++ demo-1/src/main/java/com/kentyou/ocx2024/featurelauncher/demo1/API_Launcher.java
@@ -0,0 +1,32 @@
+package com.kentyou.ocx2024.featurelauncher.demo1;
+
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ServiceLoader;
+
+import org.osgi.framework.launch.Framework;
+import org.osgi.service.featurelauncher.FeatureLauncher;
+import org.osgi.service.featurelauncher.repository.ArtifactRepository;
+
+public class API_Launcher {
+
+	public static void main(String[] args) throws Exception {
+		
+		ServiceLoader<FeatureLauncher> loader = ServiceLoader.load(FeatureLauncher.class);
+		FeatureLauncher launcher = loader.findFirst()
+			.orElseThrow(() -> new ClassNotFoundException("No Feature Launcher implementation found"));
+
+		Path targetDir = Paths.get("target");
+		
+		ArtifactRepository repository = launcher.createRepository(targetDir.resolve("repo"));
+		
+		BufferedReader feature = Files.newBufferedReader(targetDir.resolve("features/gogo.json"));
+		Framework launchFramework = launcher.launch(feature)
+			.withRepository(repository)
+			.launchFramework();
+		
+		launchFramework.waitForStop(0);
+	}
+}
